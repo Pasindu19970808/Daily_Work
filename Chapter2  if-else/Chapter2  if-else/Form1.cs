c6a39046add5a996334acee6418343defa91bb3c
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2020-11-30 22:35:03 +0800

    Add files via upload

---
+++ Chapter2  if-else/Chapter2  if-else/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter2__if_else
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true && label1.BackColor != Color.Red) 
+            {
+                label1.BackColor = Color.Red;
+            
+            }
+            else if (checkBox1.Checked == true && label1.BackColor == Color.Red)
+            {
+                label1.BackColor = Color.Blue;
+                //
+            }
+            else if (checkBox1.Checked == false) 
+            {
+                MessageBox.Show("The Box is not checked");
+            }
+        }
+    }
+}
