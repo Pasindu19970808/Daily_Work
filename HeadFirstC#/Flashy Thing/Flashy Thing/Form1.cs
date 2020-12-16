a767e8934b0337446fbf7370b1f9868df98ea58a
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2020-12-16 11:27:09 +0800

    Add files via upload

---
+++ HeadFirstC#/Flashy Thing/Flashy Thing/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flashy_Thing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                int c = 0;
+                while (c < 254 && Visible)
+                {
+
+                    this.BackColor = Color.FromArgb(255 - c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                    c++;
+                }
+
+                while (c > 0 && Visible)
+                {
+                    this.BackColor = Color.FromArgb(255 - c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                    c--;
+
+                }
+            }
+            
+            //MessageBox.Show(Convert.ToString(c));
+
+        }
+    }
+}
